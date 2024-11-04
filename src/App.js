4a58c0c8ab4eac15c3d6ef10e05e7e6698bbea23
FrantiskaM <frantiska.malucka@gmail.com>
2024-11-04 19:58:10 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Frantiska Malucka and is open-sourced on{" "}
-        <a
-          href="https://github.com/FrantiskaM/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Frantiska Malucka and is open-sourced on{" "}
+          <a
+            href="https://github.com/FrantiskaM/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
